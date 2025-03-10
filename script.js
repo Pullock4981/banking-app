8c58718b61b7fc475e1a574a5cf9b5b09958ee18
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-03-10 23:51:16 +0600

    add home page and dashboard page

---
+++ script.js
@@ -0,0 +1,18 @@
+// console.log('Hello, world!');
+
+// js for log in
+
+const logInBtn = document.getElementById('logInBtn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const mobileNumber = document.getElementById('mobile-number').value;
+        const pinNumber = document.getElementById('pin').value;
+        // console.log(mobileNumber, pinNumber);
+        if (mobileNumber.length === 11 && pinNumber == '1234') {
+            window.location.href = 'deshboard.html';
+            // console.log('yeaaaaaaaah');
+        }
+        else {
+            alert('Please enter correct mobile number and pin number');
+        }
+     }); ;
