b996bcd981abfb3fc69dd503d84b19457598ebb1
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-03-11 15:56:05 +0600

    add js for others file all

---
+++ logout.js
@@ -0,0 +1,7 @@
+// log out
+
+document.getElementById('logOutBtn')
+    .addEventListener('click', function () {
+        window.location.href = 'index.html';
+        // console.log('hi')
+    });
--- toggle.js
+++ toggle.js
@@ -5,9 +5,17 @@ document.getElementById("addMoneyBox")
     .addEventListener('click', function () {
         document.getElementById("addMoneySection").classList.remove("hidden");
         document.getElementById("history").classList.add("hidden")
+        document.getElementById("cashOutSection").classList.add("hidden");
+        document.getElementById("transferMoneySection").classList.add("hidden");
+        document.getElementById("getBonusSection").classList.add("hidden");
+        document.getElementById("payBillSection").classList.add("hidden");
+        document.getElementById("transactionSection").classList.add("hidden");
         if ("addMoneySection") {
             // console.log('hi')
             document.getElementById("addMoneyBox").style.backgroundColor = "#dff7df"
+        }
+        else {
+            document.getElementById("addMoneyBox").style.backgroundColor = "white"
         }
     })
 
@@ -16,10 +24,18 @@ document.getElementById("addMoneyBox")
 document.getElementById("cashOutBox")
     .addEventListener('click', function () {
         document.getElementById("cashOutSection").classList.remove('hidden');
-        document.getElementById("history").classList.add("hidden")
+        document.getElementById("history").classList.add("hidden");
+        document.getElementById("addMoneySection").classList.add("hidden");
+        document.getElementById("transferMoneySection").classList.add("hidden");
+        document.getElementById("getBonusSection").classList.add("hidden");
+        document.getElementById("payBillSection").classList.add("hidden");
+        document.getElementById("transactionSection").classList.add("hidden");
         if ("cashOutSection") {
             // console.log('hi')
             document.getElementById("cashOutBox").style.backgroundColor = "#dff7df"
+        }
+        else {
+            document.getElementById("cashOutBox").style.backgroundColor = "white"
         }
     })
 
@@ -29,9 +45,17 @@ document.getElementById("transferMoneyBox")
     .addEventListener('click', function () {
         document.getElementById("transferMoneySection").classList.remove('hidden');
         document.getElementById("history").classList.add("hidden")
+        document.getElementById("addMoneySection").classList.add("hidden");
+        document.getElementById("cashOutSection").classList.add("hidden");
+        document.getElementById("getBonusSection").classList.add("hidden");
+        document.getElementById("payBillSection").classList.add("hidden");
+        document.getElementById("transactionSection").classList.add("hidden");
         if ("transferMoneySection") {
             // console.log('hi')
             document.getElementById("transferMoneyBox").style.backgroundColor = "#dff7df"
+        }
+        else {
+            document.getElementById("transferMoneyBox").style.backgroundColor = "white"
         }
     })
 
@@ -41,9 +65,17 @@ document.getElementById("getBonusBox")
     .addEventListener('click', function () {
         document.getElementById("getBonusSection").classList.remove('hidden');
         document.getElementById("history").classList.add("hidden")
+        document.getElementById("addMoneySection").classList.add("hidden");
+        document.getElementById("cashOutSection").classList.add("hidden");
+        document.getElementById("transferMoneySection").classList.add("hidden");
+        document.getElementById("payBillSection").classList.add("hidden");
+        document.getElementById("transactionSection").classList.add("hidden");
         if ("getBonusSection") {
             // console.log('hi')
             document.getElementById("getBonusBox").style.backgroundColor = "#dff7df"
+        }
+        else {
+            document.getElementById("getBonusBox").style.backgroundColor = "white"
         }
     })
 
@@ -53,9 +85,17 @@ document.getElementById("payBillBox")
     .addEventListener('click', function () {
         document.getElementById("payBillSection").classList.remove('hidden');
         document.getElementById("history").classList.add("hidden")
+        document.getElementById("addMoneySection").classList.add("hidden");
+        document.getElementById("cashOutSection").classList.add("hidden");
+        document.getElementById("transferMoneySection").classList.add("hidden");
+        document.getElementById("getBonusSection").classList.add("hidden");
+        document.getElementById("transactionSection").classList.add("hidden");
         if ("payBillSection") {
             // console.log('hi')
             document.getElementById("payBillBox").style.backgroundColor = "#dff7df"
+        }
+        else {
+            document.getElementById("payBillBox").style.backgroundColor = "white"
         }
     })
 
@@ -65,8 +105,16 @@ document.getElementById("transactionBox")
     .addEventListener('click', function () {
         document.getElementById("transactionSection").classList.remove('hidden');
         document.getElementById("history").classList.add("hidden")
+        document.getElementById("addMoneySection").classList.add("hidden");
+        document.getElementById("cashOutSection").classList.add("hidden");
+        document.getElementById("transferMoneySection").classList.add("hidden");
+        document.getElementById("getBonusSection").classList.add("hidden");
+        document.getElementById("payBillSection").classList.add("hidden");
         if ("transactionSection") {
             // console.log('hi')
             document.getElementById("transactionBox").style.backgroundColor = "#dff7df"
+        }
+        else {
+            document.getElementById("transactionBox").style.backgroundColor = "white"
         }
     })
